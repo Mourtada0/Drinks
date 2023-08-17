b9388706fe49ca5aaef0df2b59edf775438b77c3
Mourtada0 <142374437+Mourtada0@users.noreply.github.com>
2023-08-17 11:24:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("Red").onclick = function(){
+
+    document.getElementById("drop").style.color = "red";
+
+}
+document.getElementById("green").onclick = function(){
+
+    document.getElementById("drop").style.color = "green";
+
+}
+document.getElementById("yellow").onclick = function(){
+
+    document.getElementById("drop").style.color = "yellow";
+
+}
+document.getElementById("burlywood").onclick = function(){
+
+    document.getElementById("drop").style.color = "burlywood";
+    var color = "burlywood";
+}
+
+document.getElementById("ColorSB").onclick = function(){
+
+    document.getElementById("ColorChoosen").innerHTML = color;
+
+}
